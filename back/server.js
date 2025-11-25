9715d84ea0e06ffb1eef0cfbc2e0bbb0720cac77
Sudhanshu Kumar <ak9006352062@gmail.com>
2025-11-25 19:45:18 +0530

    ghhghg

--- back/server.js
+++ back/server.js
@@ -23,7 +23,7 @@ if (missingVars.length > 0) {
 import authRouter from './src/routes/auth.js';
 import bookingRouter from './src/routes/boookingRoutes.js';
 import bookingConfirmationRouter from './src/routes/bookingConfirmationRoutes.js';
-import cloudinaryRoutes from './src/routes/Cloudinary.js';
+import cloudinaryRoutes from './src/routes/cloudinary.js';
 import logoRoutes from './src/routes/settings.js';
 import profileRoutes from './src/routes/profile.js';
 import airportRoutes from './src/routes/airportRoutes.js';
