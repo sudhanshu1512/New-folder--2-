419147cdc83f6042584c372a0ddff456fa3333ef
Sudhanshu Kumar <ak9006352062@gmail.com>
2025-11-26 11:55:48 +0530

    knkds

--- back/src/db.js
+++ back/src/db.js
@@ -12,7 +12,7 @@ const config = {
   password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
   database: process.env.DB_NAME,
-  port: parseInt(process.env.DB-PORT) || 1433,
+  port: parseInt(process.env.DB_PORT) || 1433,
   options: {
     encrypt: false, // For development
     trustServerCertificate: true // For local development
