a678270e67eecaf60a7b799a001d68215e21bc55
Sudhanshu Kumar <ak9006352062@gmail.com>
2025-11-29 13:10:02 +0530

    hhhhhhhhhhhhhhhh

--- back/src/db.js
+++ back/src/db.js
@@ -12,14 +12,10 @@ const config = {
   password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
   database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT) || 12360,
   options: {
     encrypt: false, // For development
-    trustServerCertificate: true ,// For local development
-    // The tunnel connection is slow. We need to tell the driver to wait longer.
-    connectTimeout: 60000, // Wait 60 seconds before giving up (Default is 15s)
-    requestTimeout: 60000, // Wait 60 seconds for a query to return
-    cancelTimeout: 60000,
-    enableArithAbort: true
+    trustServerCertificate: true // For local development
   },
   pool: {
     max: 10,
